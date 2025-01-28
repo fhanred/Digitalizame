84b0e331b5a7c5aacceaeb1271fbb26b2f5bae1e
mlobeto <mercedeslobeto@gmail.com>
2025-01-28 15:53:33 -0300

    yh

--- Digitalizame/src/Componets/Aterrizaje.jsx
+++ Digitalizame/src/Componets/Aterrizaje.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { motion } from "framer-motion";
-import ReactPlayer from "react-player";
 import logo3 from "../assets/logo3.png";
 import engranaje from "../assets/engranaje.png";
-import engranaje2 from "../assets/engranaje2.png";
+import engranaje2 from "../assets/engranaje2.png"; 
 import engranaje3 from "../assets/engranaje3.png";
 import video1 from "../assets/video1.mp4";
 import video2 from "../assets/video2.mp4";
+
 
 const Aterrizaje = () => {
   return (
@@ -24,18 +24,18 @@ const Aterrizaje = () => {
           >
             DIGITALIZAME
           </motion.h2>
-          <div className="flex flex-col md:flex-row items-center mb-4">
+          <div className="flex items-center mb-4">
             <motion.h2
-              className="text-6xl md:text-9xl font-bold text-white ml-0 md:ml-24 mt-10"
+              className="text-9xl font-bold text-white ml-24 mt-10"
               initial={{ opacity: 0, y: -50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.4 }}
             >
               SACALE
             </motion.h2>
-            <div className="flex flex-col ml-0 md:ml-8">
+            <div className="flex flex-col ml-8">
               <motion.p
-                className="text-xl md:text-2xl font-medium text-gray-300 ml-0 md:ml-60 -mb-0"
+                className="text-2xl font-medium text-gray-300 ml-80 -mb-0"
                 initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.6 }}
@@ -43,7 +43,7 @@ const Aterrizaje = () => {
                 MUESTRA LA ESENCIA DE TU MARCA
               </motion.p>
               <motion.p
-                className="text-lg md:text-xl font-medium ml-0 md:ml-60 text-gray-300 mt-0"
+                className="text-1xl font-medium ml-80 text-gray-300 mt-0"
                 initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ delay: 0.8 }}
@@ -57,7 +57,7 @@ const Aterrizaje = () => {
           </div>
           <div className="flex items-center mb-4">
             <motion.h2
-              className="text-4xl md:text-6xl font-bold text-white ml-0 md:ml-12"
+              className="text-6xl font-bold text-white ml-12"
               initial={{ opacity: 0, y: -50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 1.0 }}
@@ -66,7 +66,7 @@ const Aterrizaje = () => {
             </motion.h2>
           </div>
           <motion.h2
-            className="text-4xl md:text-6xl font-bold mb-4 ml-0 md:ml-48 text-white text-center"
+            className="text-6xl font-bold mb-4 ml-48 text-white text-center"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 1.2 }}
@@ -74,7 +74,7 @@ const Aterrizaje = () => {
             A LAS REDES
           </motion.h2>
           <motion.h2
-            className="text-4xl md:text-6xl font-bold mb-4 ml-0 md:ml-48 text-white text-center"
+            className="text-6xl font-bold mb-4 ml-48 text-white text-center"
             initial={{ opacity: 0, y: -50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ delay: 1.4 }}
@@ -103,12 +103,10 @@ const Aterrizaje = () => {
       <div className="min-h-screen bg-gradient-to-r from-black via-blue-300 via-50% via-purple-700 via-65% to-pink-600 relative">
         {/* Sección existente */}
         <div className="container mx-auto py-2">
-          <div className="flex justify-between items-center mb-8">
-            <img src={logo3} alt="Logo" className="h-32" />
-          </div>
+          
           <div className="flex flex-col space-y-2">
             <motion.h2
-              className="text-custom font-bold text-pink-400 text-center"
+              className="text-custom font-bold text-pink-700 text-center"
               initial={{ opacity: 0, y: -50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ delay: 0.2 }}
@@ -116,19 +114,16 @@ const Aterrizaje = () => {
               FIDELIZAME
             </motion.h2>
             {/* Nueva sección */}
-            <div id="next-section" className="flex flex-col md:flex-row mt-8">
-              <div className="w-full md:w-1/2 ml-0 md:ml-40">
-                <ReactPlayer
-                  url={video2}
-                  controls
-                  width="100%"
-                  height="100%"
-                  className="rounded-3xl shadow-lg"
-                />
+            <div id="next-section" className="flex flex-row mt-8">
+              <div className="w-1/5 ml-40">
+                <video controls className="w-full rounded-3xl">
+                  <source src={video2} type="video/mp4" />
+                  Tu navegador no soporta la etiqueta de video.
+                </video>
               </div>
-              <div className="w-full md:w-1/2 flex items-center justify-center">
+              <div className="w-1/2 flex items-center justify-center">
                 <motion.p
-                  className="text-xl md:text-2xl font-medium ml-0 md:ml-60 text-gray-300 mt-0"
+                  className="text-2xl font-medium ml-60 text-gray-300 mt-0"
                   initial={{ opacity: 0, y: -50 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ delay: 0.8 }}
@@ -151,33 +146,55 @@ const Aterrizaje = () => {
             transition={{ delay: 0.4 }}
           >
             <a
-              href="#third-section"
+              href="#next-section"
               className="text-blue-400 text-2xl text-center"
             >
               ↓
             </a>
           </motion.div>
         </div>
-        {/* Tercera sección */}
-        <div id="third-section" className="flex flex-col md:flex-row mt-8">
-          <div className="w-full md:w-1/2 flex items-center justify-center">
-            <p className="text-xl md:text-2xl font-medium text-gray-300">
-              Aquí va el texto que quieres mostrar a la izquierda del tercer video. Puedes agregar más contenido según sea necesario.
-            </p>
-          </div>
-          <div className="w-full md:w-1/2 ml-0 md:ml-40">
-            <ReactPlayer
-              url={video1}
-              controls
-              width="100%"
-              height="100%"
-              className="rounded-3xl shadow-lg"
-            />
-          </div>
+        <div className="min-h-screen bg-gradient-to-r from-black via-blue-300 via-50% via-purple-700 via-65% to-pink-600 relative">
+  {/* Nueva sección */}
+  <div className="container mx-auto py-2">
+    <div className="flex flex-col space-y-2">
+      <motion.h2
+        className="text-custom2 font-bold text-gray-400 text-center"
+        initial={{ opacity: 0, y: -50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2 }}
+      >
+        OPTIMIZACIÓN DE REDES SOCIALES
+      </motion.h2>
+      <div className="flex flex-row mt-8">
+        {/* Texto a la izquierda */}
+        <div className="w-1/3 flex items-center justify-center ml-10">
+  <motion.p
+    className="text-2xl font-medium text-gray-300"
+    initial={{ opacity: 0, y: -50 }}
+    animate={{ opacity: 1, y: 0 }}
+    transition={{ delay: 0.6 }}
+  >
+    Es un software que te permite entregar bonos <br /> 
+    de manera digital a tus clientes y otorgarle <br /> 
+    un descuento, obsequio o beneficio por las recompras <br />
+    que haga en tu negocio. 
+  </motion.p>
+</div>
+        {/* Video a la derecha */}
+        <div className="w-1/5 ml-80">
+          <video controls className="w-full rounded-3xl">
+            <source src={video1} type="video/mp4" />
+            Tu navegador no soporta la etiqueta de video.
+          </video>
         </div>
-        <div className="absolute h-32 top-0 right-10 mt-80 mr-80">
-          <img src={engranaje3} alt="Engranaje 2" className="h-80" />
+        <div className="absolute h-80 top-0 right-10 mt-80 mr-80">
+          <img src={engranaje3} alt="Engranaje 2" className="h-140 mr-80 mt-30" />
         </div>
+      </div>
+    </div>
+  </div>
+</div>
+        
       </div>
     </div>
   );
