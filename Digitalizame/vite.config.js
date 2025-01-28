22e8994211c5dbedb8ff0bb365b67aaa0b297608
mlobeto <mercedeslobeto@gmail.com>
2025-01-28 16:26:58 -0300

    config

--- Digitalizame/vite.config.js
+++ Digitalizame/vite.config.js
@@ -1,12 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
   },
- 
+  server: {
+    host: true
+  }
 })
